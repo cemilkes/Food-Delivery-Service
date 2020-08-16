8c215540d690891495c78579c7e02c7ec284dbd6
Cemil Kes <kes.cemil@gmail.com>
2020-08-15 17:06:43 -0700

    create podfile

---
+++ Pods/Target Support Files/Pods-Food Delivery Service-Food Delivery ServiceUITests/Pods-Food Delivery Service-Food Delivery ServiceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Food_Delivery_Service_Food_Delivery_ServiceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Food_Delivery_Service_Food_Delivery_ServiceUITests
+@end
