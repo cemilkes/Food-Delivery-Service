4dfab6117136d5f7783daec88b66c538c2e9e418
Cemil Kes <kes.cemil@gmail.com>
2020-08-15 17:03:05 -0700

    Project Added

---
+++ Food Delivery Service/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Delivery Service
+//
+//  Created by Cemil Keş on 8/15/20.
+//  Copyright © 2020 CemilKes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
