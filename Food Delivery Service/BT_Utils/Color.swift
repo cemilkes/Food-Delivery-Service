93c5b1456ba8e2b0fed078e0fd45e0ead3d3a99f
CemilKes <kes.cemil@gmail.com>
2020-08-21 19:04:53 -0700

    Color management added

--- Food Delivery Service/BT_Utils/Color.swift
+++ Food Delivery Service/BT_Utils/Color.swift
@@ -9,7 +9,114 @@
 import Foundation
 import UIKit
 
-//https://gist.github.com/sauvikatinnofied/3aad89cbaf1ea908e9b6c7f0e5829c49
-extension UIColor {
 
+// Usage Examples
+let shadowColor = Color.shadow.value
+let shadowColorWithAlpha = Color.shadow.withAlpha(0.5)
+let customColorWithAlpha = Color.custom(hexString: "#123edd", alpha: 0.25).value
+
+//https://medium.com/ios-os-x-development/a-smart-way-to-manage-colours-schemes-for-ios-applications-development-923ef976be55
+enum Color {
+    case theme
+    case border
+    case shadow
+    
+    case darkBackground
+    case lightBackground
+    case intermidiateBackground
+    
+    case darkText
+    case lightText
+    case intermidiateText
+    
+    case affirmation
+    case negation
+    
+    case custom(hexString: String, alpha: Double)
+    
+    func withAlpha(_ alpha: Double) -> UIColor {
+        return self.value.withAlphaComponent(CGFloat(alpha))
+    }
 }
+extension Color {
+    
+    var value: UIColor {
+        var instanceColor = UIColor.clear
+        
+        switch self {
+        case .border:
+            instanceColor = UIColor(hexString: "#333333")
+        case .theme:
+            instanceColor = UIColor(hexString: "#ffcc00")
+        case .shadow:
+            instanceColor = UIColor(hexString: "#ccccc")
+        case .darkBackground:
+            instanceColor = UIColor(hexString: "#999966")
+        case .lightBackground:
+            instanceColor = UIColor(hexString: "#cccc66")
+        case .intermidiateBackground:
+            instanceColor = UIColor(hexString: "#cccc99")
+        case .darkText:
+            instanceColor = UIColor(hexString: "#333333")
+        case .intermidiateText:
+            instanceColor = UIColor(hexString: "#999999")
+        case .lightText:
+            instanceColor = UIColor(hexString: "#cccccc")
+        case .affirmation:
+            instanceColor = UIColor(hexString: "#00ff66")
+        case .negation:
+            instanceColor = UIColor(hexString: "#ff3300")
+        case .custom(let hexValue, let opacity):
+            instanceColor = UIColor(hexString: hexValue).withAlphaComponent(CGFloat(opacity))
+        }
+        return instanceColor
+    }
+}
+
+extension UIColor {
+    /**
+     Creates an UIColor from HEX String in "#363636" format
+     
+     - parameter hexString: HEX String in "#363636" format
+     
+     - returns: UIColor from HexString
+     */
+    convenience init(hexString: String) {
+        
+        let hexString: String = (hexString as NSString).trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner          = Scanner(string: hexString as String)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:1)
+    }
+}
+
+/*
+ All applications maintains a colour scheme though out every screens. This ensures the consistency over the entire application. Broadly, we can classify them under the following categories.
+ Theme Colour. Colours on Navigation Bar, Button Titles, Progress Indicator etc.
+ Border Colour: Hair line separators in between views.
+ Shadow Colour: Shadow colours for card like design.
+ Dark Background Colour: Dark background colour to group UI components with light colour.
+ Light Background Colour: Light background colour to group UI components with dark colour.
+ Intermediate Background Colour: Used for grouping UI elements with some other colour scheme.
+ Dark Text Colour:
+ Light Text Colour:
+ Intermediate Text Colour:
+ Affirmation: Colour to show success, something right for user.
+ Negation: Colour to show error, some danger zones for user.
+ */
