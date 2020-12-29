f87a6f143f49c215cfdf820da9e3c267eecdab37
CemilKes <kes.cemil@gmail.com>
2020-12-28 21:05:30 -0800

    Commit changes

---
+++ Food Delivery Service/Order-History/Controller/FavouriteController.swift
@@ -0,0 +1,29 @@
+//
+//  FavouriteController.swift
+//  Food Delivery Service
+//
+//  Created by Mr Kes on 12/28/20.
+//  Copyright © 2020 CemilKes. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
